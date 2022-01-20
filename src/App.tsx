8bc5923b06cbedb89453bbc37042efbd923a0707
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2022-01-20 09:55:09 -0500

    ✨ update: working Game component, lifting states

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,37 @@
-import { useState } from "react";
+import { useState, ReactElement } from "react";
 
 function App() {
   return (
     <div className="w-screen h-screen bg-red-300">
       <h1 className="text-3xl font-bold underline">Daniel's Tic Tac Toe App</h1>
-      <Board />
+      <Game />
     </div>
   );
 }
 
-
 const Square = (props: { value: string; onSquareClick: () => void }) => {
   return (
-    <>
-      <button
-        className="w-32 h-32 bg-transparent hover:bg-blue-500 text-blue-700 font-bold hover:text-white border-2 border-blue-500 px-3 py-5 hover:border-transparent"
-        onClick={props.onSquareClick}
-      >
-        <span className="text-[60px] font-extrabold">{props.value}</span>
-      </button>
-    </>
+    <button
+      className="w-32 h-32 bg-transparent hover:bg-blue-500 text-blue-700 font-bold hover:text-white border-2 border-blue-500 px-3 py-5 hover:border-transparent"
+      onClick={props.onSquareClick}
+    >
+      <span className="text-[60px] font-extrabold">{props.value}</span>
+    </button>
   );
 };
 
-const Board = () => {
-  const [xIsNext, setXIsNext] = useState(true); // order tracking
-  const [squares, setSquares] = useState(Array(9).fill("")); // board state
-
+const Board = ({
+  xIsNext,
+  squares,
+  setXIsNext,
+  setSquares,
+}: {
+  xIsNext: boolean;
+  squares: string[];
+  setXIsNext: React.Dispatch<React.SetStateAction<boolean>>;
+  setSquares: React.Dispatch<React.SetStateAction<string[]>>;
+}): ReactElement => {
+  // get the winner
   const calculateWinner = (squares: string[]): string | null => {
     const lines = [
       [0, 1, 2],
@@ -41,14 +46,18 @@ const Board = () => {
 
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] == squares[b] && squares[a] == squares[c]) {
-        console.log(squares[a]);
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
         return squares[a];
       }
     }
     return null;
   };
 
+  // handle click
   const handleClick = (i: number) => {
     if (squares[i] || calculateWinner(squares)) return; // so it doesn't overwrite existing squares
 
@@ -64,7 +73,7 @@ const Board = () => {
   };
 
   return (
-    <div className="flex">
+    <div className="flex mt-16">
       <div>
         <div className="flex items-center">
           <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
@@ -83,9 +92,27 @@ const Board = () => {
         </div>
       </div>
       <div className="ml-16">
-        <h1>Winner is X</h1>
+        <h1>
+          {calculateWinner(squares)
+            ? `Winner is ${calculateWinner(squares)}`
+            : "No Winner Yet"}
+        </h1>
       </div>
     </div>
+  );
+};
+
+const Game = () => {
+  const [xIsNext, setXIsNext] = useState(true); // order tracking
+  const [squares, setSquares] = useState(Array(9).fill("")); // board state
+
+  return (
+    <Board
+      xIsNext={xIsNext}
+      squares={squares}
+      setXIsNext={setXIsNext}
+      setSquares={setSquares}
+    />
   );
 };
 
